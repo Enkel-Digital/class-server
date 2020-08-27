6433216fd87c49e140327d5225f52d3ad47ba1fb
Zheng Zikang <zhengzikangzzz@gmail.com>
2020-08-27 17:06:30 +0800

    [billing] created routes to create account link

--- billing/src/routes/partner.js
+++ billing/src/routes/partner.js
@@ -1,10 +1,51 @@
 const express = require("express");
 const router = express.Router();
+const db = require("./../utils/db.js");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// const { getStripePartnerID } = require("../db/getPartner");
+
+// @todo This might be an issue if customer is deleted on stripe but not deleted from database.
+async function partnerExists(partnerID) {
+  return Boolean(
+    (
+      await db
+        .collection("billingPartnerAccounts")
+        .doc(partnerID.toString()) // As userAccountID is a number, WE MUST CONVERT it to string first, as firestore NEEDS string for document path!!!
+        .get()
+    ).data()
+  );
+}
+async function getStripePartnerID(partnerAccountID) {
+  const snapshot = await db
+    .collection("billingPartnerAccounts")
+    .doc(partnerAccountID.toString())
+    .get();
+
+  if (snapshot.empty) return;
+
+  return snapshot.data().partnerID;
+}
 
 /**
- * Create a new customer
- * @name POST /partner/create
+ * Checks if partner object exists for this partnerID
+ * @name GET /partner/exists/:partnerID
+ * @returns {object} Boolean
+ */
+router.get("/exists/:partnerID", async (req, res) => {
+  try {
+    const { partnerID } = req.params;
+
+    return res
+      .status(200)
+      .json({ success: true, exists: await partnerExists(partnerID) });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+/**
+ * Create a new partner Connect account
+ * @name POST /partner
  * @param {string} country  country of the partner business
  * @param {string} email    email of the partner
  * @param {string} businessType business type of the partner
@@ -12,9 +53,16 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
  * @param {string} individual info of the person represented by the account
  * @returns {object} Boolean
  */
-router.post("/create", express.json(), async (req, res) => {
+router.post("/", express.json(), async (req, res) => {
   try {
-    const { country, email, businessType, company, individual } = req.body;
+    const {
+      partnerID,
+      country,
+      email,
+      businessType,
+      // company,
+      // individual,
+    } = req.body;
 
     const accountType = "standard";
     const account = await stripe.accounts.create({
@@ -23,15 +71,71 @@ router.post("/create", express.json(), async (req, res) => {
       email: email,
       business_type: businessType,
     });
-    
-      // save stripe's customer.id as customerID in db
-      await db
-      .collection("billingCustomerAccounts")
-      .doc(userAccountID.toString()) // As userAccountID is a number, WE MUST CONVERT it to string first, as firestore NEEDS string for document path!!!
-      .set({ customerID: customer.id });
 
-      
+    await db
+      .collection("billingPartnerAccounts")
+      .doc(partnerID.toString()) // As partnerID is a number, WE MUST CONVERT it to string first, as firestore NEEDS string for document path!!!
+      .set({ partnerID: account.id });
+
     res.status(201).json({ success: true });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+/**
+ * Create an account link
+ * @name POST /partner/link/:partnerID
+ * @param {string} account the identifier of the account to create an account link for.
+ * @param {string} refresh_url The URL that the user will be redirected to if the account link is no longer valid.
+ * @param {string} return_url the URL that the user will be redirected to upon leaving or completing the linked flow.
+ * @param {string} type The type of account link the user is requesting. Possible values are account_onboarding or account_update.
+ * @returns {object} Boolean
+ */
+router.post("/link/:partnerID", express.json(), async (req, res) => {
+  try {
+    // eslint-disable-next-line camelcase
+    const { refresh_url, return_url, type } = req.body;
+    const { partnerID } = req.params;
+    const stripePartnerID = await getStripePartnerID(partnerID);
+
+    const accountLink = await stripe.accountLinks.create({
+      account: stripePartnerID,
+      refresh_url: "https://google.com",
+      return_url: "https://youtube.com",
+      type: "account_onboarding",
+    });
+
+    res.status(201).json({ success: true, url: accountLink.url });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+/**
+ * Create an account link to update partner info
+ * @name POST /partner/link/:partnerID
+ * @param {string} account the identifier of the account to create an account link for.
+ * @param {string} refresh_url The URL that the user will be redirected to if the account link is no longer valid.
+ * @param {string} return_url the URL that the user will be redirected to upon leaving or completing the linked flow.
+ * @param {string} type The type of account link the user is requesting. Possible values are account_onboarding or account_update.
+ * @returns {object} Boolean
+ */
+router.post("/link/update/:partnerID", express.json(), async (req, res) => {
+  try {
+    // eslint-disable-next-line camelcase
+    const { refresh_url, return_url, type } = req.body;
+    const { partnerID } = req.params;
+    const stripePartnerID = await getStripePartnerID(partnerID);
+
+    const accountLink = await stripe.accountLinks.create({
+      account: stripePartnerID,
+      refresh_url: "https://google.com",
+      return_url: "https://youtube.com",
+      type: "account_update",
+    });
+
+    res.status(201).json({ success: true, url: accountLink.url });
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
   }
