b650fca82049fa3f7e7c4a2dcd56a7b4d6565aff
Jaime <jaimeloeuf@gmail.com>
2020-10-13 16:51:52 +0800

    gitpkg

---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("start");
+
+console.log(require("./tags"));
+console.log(require("./tags").initialize);
+console.log(require("./tags").initialize());
+console.log(require("./tags"));
---
+++ test2.js
@@ -0,0 +1,5 @@
+console.log("start 2");
+
+console.log(require("./tags"));
+require("./test");
+console.log(require("./tags").class.get(1));
